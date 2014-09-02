49daacd15feca69b2ff5cacbfdfa90fe9c224c78
rafaelmuk <andrade.muk@gmail.com>
2014-09-02 18:42:22 -0300

    começando os trabalhos

--- src/compilador.cpp
+++ src/compilador.cpp
@@ -1,41 +1,17 @@
-// "Copyright [year] <Copyright Owner>"
 #include <iostream>
 #include <fstream>
 
 char getChar(std::ifstream & fin) {
-  return 'C';
-  
-  // faguin_28 picagrroooooooooossa
-  
+  return 'C';  
 }
+
 int main() {
   std::ifstream fin("file.lug", std::fstream::in);
   char ch;
 
-  //ch = getChar(fin);
-  // printf(" %c",ch);
-  
-  // getChar(fin);
-  //  std::cout << getChar(fin) << std::endl;
-
   while (!fin.eof()) {
      std::cout << getChar(fin) << std::endl;
   }
-
-  //  ch = getChar(fin);
-// testando rafael
-  // teste acertando erros
-
-
-
-  do {
-    std::cout << "gay" << std::endl;
-  }
-  
-
- for(int i;i<n;i++) {
-std::cout << "fagner gay" << std::endl;
- }
 
   return 0;
 }
