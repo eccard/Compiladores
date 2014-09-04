0e5f33e50b8c1c49854a0fe674fac43c292c2b25
rafaelmuk <andrade.muk@gmail.com>
2014-09-04 01:00:12 -0300

    aceitando todos os tokens

--- src/compilador.cpp
+++ src/compilador.cpp
@@ -8,7 +8,7 @@ typedef struct Token {
 
 }Token;
 
-  
+
   
 Token getToken(std::ifstream & fin, int *linha) {
   Token token;
@@ -23,36 +23,134 @@ Token getToken(std::ifstream & fin, int *linha) {
   ch= fin.get();
 
   if (ch == ';') {
-    token.info[0]= ';';
+	  *linha++;
+    token.info[0]= ch;
     return token;
   }
-  
-  while ( ch!= ' ' && fin.good() ) {
-    if ( ch!= ';' ) {
-      token.info[cont]=ch;
-      cont++;
-    } else {
-      fin.unget();
-      return token;
-    }
-
-    if (ch == '\n')
-      linha++;
-    ch= fin.get();
-    
+  if (ch == '=') {
+	  linha++;
+    token.info[0]= ch;
+    return token;
   }
+  if (ch == '<') {
+	  linha++;
+    token.info[0]= ch;
+    return token;
+  }
+  if (ch == '>') {
+	  linha++;
+    token.info[0]= ch;
+    return token;
+  }
+  if (ch == ')') {
+	  linha++;
+    token.info[0]= ch;
+    return token;
+  }
+  if (ch == '(') {
+	  linha++;
+    token.info[0]= ch;
+    return token;
+  }
+  if (ch == '[') {
+	  linha++;
+    token.info[0]= ch;
+    return token;
+  }
+  if (ch == ']') {
+	  linha++;
+    token.info[0]= ch;
+    return token;
+  }
+  if (ch == '!') {
+	  linha++;
+    token.info[0]= ch;
+    return token;
+  }
+  if (ch == '+') {
+	  linha++;
+    token.info[0]= ch;
+    return token;
+  }
+  if (ch == '-') {
+	  linha++;
+    token.info[0]= ch;
+    return token;
+  }
+  if (ch == '*') {
+	  linha++;
+    token.info[0]= ch;
+    return token;
+  }
+  if (ch == '/') {
+	  linha++;
+    token.info[0]= ch;
+    return token;
+  }
+  if (ch == ',') {
+	  linha++;
+    token.info[0]= ch;
+    return token;
+  }
+  if (ch == '\n') {
+	  linha++;
+    ch= fin.get(); 
+  }
+  if (ch == ' ') {
+	  linha++;
+    ch= fin.get(); 
+  }
+  if (ch == ':'){
+   	    token.info[0]=ch;
+	    cont++;
+	    *linha++;
+	    ch= fin.get(); 
+	  
+	  if (ch == '='){
+		token.info[1]= ch;
+		return token;
+	  }
+	  fin.unget();
+	return token;
+	}
+  
+  
+  
+  while ( ch!= ' ' && fin.good()) {
+    if ( ch!=';' && ch!='=' && ch!='<' && ch!=',' &&
+		 ch!='>' && ch!='(' && ch!=')' && ch!='[' && 
+         ch!=']' && ch!='!' && ch!='+' && ch!='-' && 
+         ch!='*' && ch!='/' && ch!=':' && ch!= '\n') {
+		   token.info[cont]=ch;
+		  cont++;
+		}else {
+		  fin.unget();
+		  return token;
+		}	
+       
+    if(ch == '='){
+       fin.unget();
+       return token;
+      }
+
+   
+    ch= fin.get(); 
+  }
+  *linha++;
   
   token.info[cont]='\0';
   token.linha= *linha;
   return token;
 }
 
+
 int main() {
   std::ifstream fin("file.lug", std::fstream::in);
   char ch;
   int linha= 0;
   while (fin.good()) {
-    std::cout << getToken(fin, &linha).info << std::endl;
+    std::cout << getToken(fin, &linha).info<< std::endl;
   }
+  
   return 0;
 }
